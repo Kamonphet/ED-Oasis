eebc7ef78040405bd575e07ddfb938a53199804a
Kamonphet <62428925+Kamonphet@users.noreply.github.com>
2023-04-01 00:26:10 +0700

    Add files via upload

--- src/sql/sorntampan.sql
+++ src/sql/sorntampan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 17, 2023 at 07:39 AM
+-- Generation Time: Mar 31, 2023 at 07:25 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -60,7 +60,7 @@ CREATE TABLE `lesson_plan` (
   `lp_name` varchar(255) DEFAULT NULL,
   `lp_unit` int(11) DEFAULT NULL,
   `lp_title` varchar(255) DEFAULT NULL,
-  `lp_info` varchar(500) DEFAULT NULL,
+  `lp_info` longtext DEFAULT NULL,
   `lp_file_id` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -74,7 +74,8 @@ INSERT INTO `lesson_plan` (`lp_id`, `user_id`, `lp_name`, `lp_unit`, `lp_title`,
 (4, 'T00001', 'แผนคอม', 1, ' Ut  Ut enim ffdgdfdf', 'ㄴㄹㅊㄴㅇㄹㄴำดำ่ท', '/CED312/upload/กติกาการเล่น Coding Card.pdf'),
 (5, 'T00002', 'ㄶㄴㅇㅎㄴㅇㅎ', 5, ' Ut enim ffdgdfLorem ipsum dolor sit amet,', 'ㄴㅋㅎㄶㄶㄶㄹㄴㅇㅎㅀㅀ', '/CED312/upload/Doc4.pdf'),
 (9, 'T00001', 'แผนไทย', 2, 'หลักษาไทย', 'วยาสวหไกไดำดำ', '/CED312/upload/marisa2020,+{$userGroup},+4.+นิรันดร์+ชัยวิเศษ+38-52.pdf'),
-(10, 'T00001', 'แผนไทย', 1, 'หลักษาไทย', 'lorem ipsum', '/CED312/upload/_  Story board GOF.pdf');
+(10, 'T00001', 'แผนไทย', 1, 'หลักษาไทย', 'lorem ipsum', '/CED312/upload/_  Story board GOF.pdf'),
+(11, 'T0003', 'test', 1, 'อุปกรณ์คอม', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p', '');
 
 -- --------------------------------------------------------
 
@@ -103,12 +104,61 @@ INSERT INTO `post` (`post_id`, `lp_id`, `post_date`, `post_detail`, `post_file_i
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `required`
+--
+
+CREATE TABLE `required` (
+  `req_id` int(11) NOT NULL,
+  `user_id` varchar(10) DEFAULT NULL,
+  `req_type` int(11) DEFAULT NULL,
+  `req_detail` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `required`
+--
+
+INSERT INTO `required` (`req_id`, `user_id`, `req_type`, `req_detail`) VALUES
+(1, 'S0004', 6, 'เก่งแล้ว'),
+(2, 'S0004', 8, 'เก่ง ๆๆๆ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `required_type`
+--
+
+CREATE TABLE `required_type` (
+  `rt_id` int(11) NOT NULL,
+  `rt_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `required_type`
+--
+
+INSERT INTO `required_type` (`rt_id`, `rt_name`) VALUES
+(1, 'ฟังบรรยาย'),
+(2, 'การอภิปรายกลุ่ม'),
+(3, 'ใช้เกม'),
+(4, 'การสาธิต'),
+(5, 'การแสดงละคร'),
+(6, 'สถานการณ์จำลอง'),
+(7, 'การทดลอง'),
+(8, 'การแสดงบทบาทสมมติ'),
+(9, 'ใช้ศูนย์การเรียน'),
+(10, 'การไปทัศนศึกษา'),
+(11, 'การใช้กรณีตัวอย่าง');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `id` int(10) UNSIGNED NOT NULL,
-  `user_id` varchar(255) DEFAULT NULL,
+  `user_id` varchar(10) DEFAULT NULL,
   `user_sname` varchar(255) DEFAULT NULL,
   `user_lname` varchar(255) DEFAULT NULL,
   `class_id` int(11) DEFAULT NULL,
@@ -124,12 +174,15 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `user_id`, `user_sname`, `user_lname`, `class_id`, `subject_id`, `user_email`, `user_tell`, `user_password`, `user_tier`) VALUES
-(1, 'S00001', 'you', 'me', 1, NULL, 'dfsdfSdfdsg@gmail.com', '0999999999', '$2y$10$JzEIMTUmlxY1WxjjlCjyAe28pkSh8BN.oJ.GDkmAUp97SV4lAEdYm', 'student'),
-(2, 'T00001', 'phet4', 'kamonphet', 1, 1, 'afhtygjytb@gmail.com', '0123456789', '$2y$10$/VfhYfrhJ2767qJcIrUiCer7CzfyxxiCf7unmKN0nXwwp/mDG8Jku', 'teacher'),
-(3, 'S00002', 'd', 'a', 2, NULL, 'fsdfiosdhfik@gmail.com', '0147258369', '$2y$10$p0EuosVS95Bkfdn7wOZBkOK.pm8CyWugaGFA1zPkCxT2PpjEVjURC', 'student'),
-(4, 'T00002', 'soy', 'yee', 2, 3, 'iofgshjgmlk@gmail.com', '0369852147', '$2y$10$NSu9Cr4WQTZkDbS23odL9O76tycrqZWnxHbr35F5vlbvy1xDvcjh6', 'teacher'),
-(5, 'T00003', 'สาจัง', 'ลาละ', 6, 7, 'user@email.com', '0889854214', '$2y$10$Kbhci2S0EWGvVUX71ZpfReZJl9TvxgDreCmuHSGvOKzGYNockZlgO', 'student'),
-(6, 'T00004', 'phet', 'ลาละ', 2, 9, 'user@email.com', '0889854214', '$2y$10$FwwjyZeUMUKzUXkZ9PsFseuULAI8a9AnBN8PrDmJ9pe7k1n18TQIK', 'teacher');
+(1, 'S0001', 'you', 'me', 1, NULL, 'dfsdfSdfdsg@gmail.com', '0999999999', '$2y$10$JzEIMTUmlxY1WxjjlCjyAe28pkSh8BN.oJ.GDkmAUp97SV4lAEdYm', 'student'),
+(2, 'T0001', 'phet4', 'kamonphet', 1, 1, 'afhtygjytb@gmail.com', '0123456789', '$2y$10$/VfhYfrhJ2767qJcIrUiCer7CzfyxxiCf7unmKN0nXwwp/mDG8Jku', 'teacher'),
+(3, 'S0002', 'd', 'a', 2, NULL, 'fsdfiosdhfik@gmail.com', '0147258369', '$2y$10$p0EuosVS95Bkfdn7wOZBkOK.pm8CyWugaGFA1zPkCxT2PpjEVjURC', 'student'),
+(4, 'T0002', 'soy', 'yee', 2, 3, 'iofgshjgmlk@gmail.com', '0369852147', '$2y$10$FwwjyZeUMUKzUXkZ9PsFseuULAI8a9AnBN8PrDmJ9pe7k1n18TQIK', 'teacher'),
+(5, 'S0003', 'สาจัง', 'ลาละ', 6, 7, 'user@email.com', '0889854214', '$2y$10$Kbhci2S0EWGvVUX71ZpfReZJl9TvxgDreCmuHSGvOKzGYNockZlgO', 'student'),
+(6, 'T0003', 'phet', 'ลาละ', 2, 9, 'user@email.com', '0889854214', '$2y$10$FwwjyZeUMUKzUXkZ9PsFseuULAI8a9AnBN8PrDmJ9pe7k1n18TQIK', 'teacher'),
+(7, 'A0001', 'Admin', 'Admin', NULL, NULL, 'userAdmin@email.com', NULL, '$2y$10$U0KCu54NrJ4sRCH8TRb4SeueWV2EzSJlGj/eb4.hOb32dAwL7UoFu', 'admin'),
+(8, 'S0004', 'สมชาย', 'เรียนดี', 5, 14, 'user@email.com', '0978452144', '$2y$10$wxEPaSRwSQphNtMyph3ZaeM4PUouTiT1.Nb6WiOxiHNMAocBH50yu', 'student'),
+(9, 's0005', 'Test', 'Test', 5, 7, 'Test@gmail.com', '8585858585', '$2y$10$TPH6V38jM6mI0mRwqoluaezGV47VURyC8lcKJrpGbzm14C2yj5ITq', 'student');
 
 -- --------------------------------------------------------
 
@@ -209,6 +262,18 @@ ALTER TABLE `post`
   ADD PRIMARY KEY (`post_id`);
 
 --
+-- Indexes for table `required`
+--
+ALTER TABLE `required`
+  ADD PRIMARY KEY (`req_id`);
+
+--
+-- Indexes for table `required_type`
+--
+ALTER TABLE `required_type`
+  ADD PRIMARY KEY (`rt_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -240,7 +305,7 @@ ALTER TABLE `evaluation_form`
 -- AUTO_INCREMENT for table `lesson_plan`
 --
 ALTER TABLE `lesson_plan`
-  MODIFY `lp_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `lp_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `post`
@@ -249,10 +314,16 @@ ALTER TABLE `post`
   MODIFY `post_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT for table `required`
+--
+ALTER TABLE `required`
+  MODIFY `req_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user_class_teach`
